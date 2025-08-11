f4a0e81641222e49e36d6f5e1386f0ea1bf093b0
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-11 13:04:38 +0300

    feat(daraja): add B2CAccountTopUp method for funding B2C shortcode
    - Introduces B2CTopUpRequest and B2CTopUpResponse structs with JSON mappings
    - Implements validation for CommandID and identifier types
    - Sends POST request to B2B endpoint to perform B2C account top-up
    - Parses and returns API response details

---
+++ mpesa/pkg/daraja/b2c_topup.go
@@ -0,0 +1,56 @@
+package daraja
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type B2CTopUpRequest struct {
+	Initiator             string `json:"Initiator"`
+	SecurityCredential    string `json:"SecurityCredential"`
+	CommandID             string `json:"CommandID"`
+	SenderIdentifierType  string `json:"SenderIdentifierType"`
+	RecieverIdentifierType string `json:"RecieverIdentifierType"`
+	Amount                string `json:"Amount"`
+	PartyA                string `json:"PartyA"`
+	PartyB                string `json:"PartyB"`
+	AccountReference      string `json:"AccountReference,omitempty"`
+	Requester             string `json:"Requester,omitempty"`
+	Remarks               string `json:"Remarks"`
+	QueueTimeOutURL       string `json:"QueueTimeOutURL"`
+	ResultURL             string `json:"ResultURL"`
+}
+
+type B2CTopUpResponse struct {
+	OriginatorConversationID string `json:"OriginatorConversationID"`
+	ConversationID           string `json:"ConversationID"`
+	ResponseCode             string `json:"ResponseCode"`
+	ResponseDescription      string `json:"ResponseDescription"`
+}
+
+func (s *Service) B2CAccountTopUp(req B2CTopUpRequest) (*B2CTopUpResponse, error) {
+	if req.CommandID != "BusinessPayToBulk" {
+		return nil, fmt.Errorf("invalid CommandID: only BusinessPayToBulk is allowed for this API")
+	}
+
+	if req.SenderIdentifierType != "4" {
+		return nil, fmt.Errorf("invalid SenderIdentifierType: only 4 is allowed for this API")
+	}
+
+	if req.RecieverIdentifierType != "4" {
+		return nil, fmt.Errorf("invalid RecieverIdentifierType: only 4 is allowed for this API")
+	}
+
+	respBody, err := s.makeRequest(http.MethodPost, b2bURL, req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make B2C Account Top Up request: %w", err)
+	}
+
+	var response B2CTopUpResponse
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return nil, fmt.Errorf("failed to parse B2C Account Top Up response: %w", err)
+	}
+
+	return &response, nil
+}
