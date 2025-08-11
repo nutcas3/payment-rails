82f92c17855bcb779e41b3ae008a01ab51f510ab
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-11 13:16:35 +0300

    feat(mpesa): add RemitTax API client wrapper
    - Introduced TaxRemittanceRequest and TaxRemittanceResponse structs
    - Implemented Client.RemitTax to wrap daraja.RemitTax call
    - Set fixed values for CommandID (PayTaxToKRA), Sender/Receiver types (4), and PartyB (KRA shortcode 572572)
    - Mapped request/response fields between mpesa and daraja layers

---
+++ mpesa/tax_remittance.go
@@ -0,0 +1,52 @@
+package mpesa
+
+import (
+	"payment-rails/mpesa/pkg/daraja"
+)
+
+type TaxRemittanceRequest struct {
+	Initiator          string // The M-Pesa API operator username
+	SecurityCredential string // The encrypted password of the M-Pesa API operator
+	Amount             string // The transaction amount
+	PartyA             string // Your shortcode (from which money will be deducted)
+	AccountReference   string // The payment registration number (PRN) issued by KRA
+	Remarks            string // Any additional information to be associated with the transaction
+	QueueTimeOutURL    string // URL for timeout notifications
+	ResultURL          string // URL for result notifications
+}
+
+type TaxRemittanceResponse struct {
+	OriginatorConversationID string
+	ConversationID           string
+	ResponseCode             string
+	ResponseDescription      string
+}
+
+func (c *Client) RemitTax(req TaxRemittanceRequest) (*TaxRemittanceResponse, error) {
+	internalReq := daraja.TaxRemittanceRequestBody{
+		Initiator:             req.Initiator,
+		SecurityCredential:    req.SecurityCredential,
+		CommandID:             daraja.CommandIDPayTaxToKRA, // Only PayTaxToKRA is allowed for this API
+		SenderIdentifierType:  "4",                         // Only type 4 is allowed for this API
+		RecieverIdentifierType: "4",                        // Only type 4 is allowed for this API
+		Amount:                req.Amount,
+		PartyA:                req.PartyA,
+		PartyB:                "572572",                    // Only 572572 (KRA) is allowed for this API
+		AccountReference:      req.AccountReference,
+		Remarks:               req.Remarks,
+		QueueTimeOutURL:       req.QueueTimeOutURL,
+		ResultURL:             req.ResultURL,
+	}
+
+	resp, err := c.Service.RemitTax(internalReq)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TaxRemittanceResponse{
+		OriginatorConversationID: resp.OriginatorConversationID,
+		ConversationID:           resp.ConversationID,
+		ResponseCode:             resp.ResponseCode,
+		ResponseDescription:      resp.ResponseDescription,
+	}, nil
+}
