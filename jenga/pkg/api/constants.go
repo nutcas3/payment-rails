0cef67e7d6fcb68dfabb58ec22cccbf0c5a14a5f
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-22 14:02:54 +0300

    feat(api): add transfer and wallet type constants

---
+++ jenga/pkg/api/constants.go
@@ -0,0 +1,14 @@
+package api
+
+const (
+	TransferTypeEFT   = "EFT"      // Electronic Funds Transfer
+	TransferTypePESALINK = "PESALINK" // PesaLink
+	TransferTypeRTGS  = "RTGS"     // Real-Time Gross Settlement
+	TransferTypeSWIFT = "SWIFT"    // SWIFT international transfer
+)
+
+const (
+	WalletTypeMPESA = "M-PESA"
+	WalletTypeAIRTEL = "AIRTEL"
+	WalletTypeEQUITEL = "EQUITEL"
+)
