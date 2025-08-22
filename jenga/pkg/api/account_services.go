d854c42ee286339325b44e3e40efdd49ef2addbb
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-22 13:44:07 +0300

    feat(api): add account balance, mini statement, and full statement endpoints
    Implemented GetAccountBalance, GetMiniStatement, and GetFullStatement methods in Client.
    
    Added query parameter handling for accountType, currencyCode, fromDate, and toDate.
    
    Integrated request signing with AccountID.
    
    Centralized query string construction using joinQueryParams.
    
    Improved response unmarshalling with proper error wrapping.

---
+++ jenga/pkg/api/account_services.go
@@ -0,0 +1,122 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+const (
+	accountBalanceEndpoint = "/account-api/v3.0/accounts/balances"
+	miniStatementEndpoint  = "/account-api/v3.0/accounts/ministatement"
+	fullStatementEndpoint  = "/account-api/v3.0/accounts/fullstatement"
+)
+
+func (c *Client) GetAccountBalance(req AccountBalanceRequest) (*AccountBalanceResponse, error) {
+	if req.CountryCode == "" || req.AccountID == "" {
+		return nil, fmt.Errorf("countryCode and accountId are required")
+	}
+
+	endpoint := fmt.Sprintf("%s/%s/%s", accountBalanceEndpoint, req.CountryCode, req.AccountID)
+	
+	queryParams := make([]string, 0)
+	if req.AccountType != "" {
+		queryParams = append(queryParams, fmt.Sprintf("accountType=%s", req.AccountType))
+	}
+	if req.CurrencyCode != "" {
+		queryParams = append(queryParams, fmt.Sprintf("currencyCode=%s", req.CurrencyCode))
+	}
+	
+	if len(queryParams) > 0 {
+		endpoint = fmt.Sprintf("%s?%s", endpoint, joinQueryParams(queryParams))
+	}
+
+	signatureData := req.AccountID
+
+	respBody, err := c.SendRequest(http.MethodGet, endpoint, nil, signatureData)
+	if err != nil {
+		return nil, err
+	}
+
+	var response AccountBalanceResponse
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return nil, fmt.Errorf("error parsing account balance response: %w", err)
+	}
+
+	return &response, nil
+}
+
+func (c *Client) GetMiniStatement(req MiniStatementRequest) (*MiniStatementResponse, error) {
+	if req.CountryCode == "" || req.AccountID == "" {
+		return nil, fmt.Errorf("countryCode and accountId are required")
+	}
+
+	endpoint := fmt.Sprintf("%s/%s/%s", miniStatementEndpoint, req.CountryCode, req.AccountID)
+	
+	queryParams := make([]string, 0)
+	if req.AccountType != "" {
+		queryParams = append(queryParams, fmt.Sprintf("accountType=%s", req.AccountType))
+	}
+	if req.CurrencyCode != "" {
+		queryParams = append(queryParams, fmt.Sprintf("currencyCode=%s", req.CurrencyCode))
+	}
+	
+	if len(queryParams) > 0 {
+		endpoint = fmt.Sprintf("%s?%s", endpoint, joinQueryParams(queryParams))
+	}
+
+	signatureData := req.AccountID
+
+	respBody, err := c.SendRequest(http.MethodGet, endpoint, nil, signatureData)
+	if err != nil {
+		return nil, err
+	}
+
+	var response MiniStatementResponse
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return nil, fmt.Errorf("error parsing mini statement response: %w", err)
+	}
+
+	return &response, nil
+}
+
+func (c *Client) GetFullStatement(req FullStatementRequest) (*FullStatementResponse, error) {
+	if req.CountryCode == "" || req.AccountID == "" || req.FromDate == "" || req.ToDate == "" {
+		return nil, fmt.Errorf("countryCode, accountId, fromDate, and toDate are required")
+	}
+
+	endpoint := fmt.Sprintf("%s/%s/%s", fullStatementEndpoint, req.CountryCode, req.AccountID)
+	
+	queryParams := []string{
+		fmt.Sprintf("fromDate=%s", req.FromDate),
+		fmt.Sprintf("toDate=%s", req.ToDate),
+	}
+	
+	if req.AccountType != "" {
+		queryParams = append(queryParams, fmt.Sprintf("accountType=%s", req.AccountType))
+	}
+	if req.CurrencyCode != "" {
+		queryParams = append(queryParams, fmt.Sprintf("currencyCode=%s", req.CurrencyCode))
+	}
+	
+	endpoint = fmt.Sprintf("%s?%s", endpoint, joinQueryParams(queryParams))
+
+	signatureData := req.AccountID
+
+	respBody, err := c.SendRequest(http.MethodGet, endpoint, nil, signatureData)
+	if err != nil {
+		return nil, err
+	}
+
+	var response FullStatementResponse
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return nil, fmt.Errorf("error parsing full statement response: %w", err)
+	}
+
+	return &response, nil
+}
+
+func joinQueryParams(params []string) string {
+	return strings.Join(params, "&")
+}
