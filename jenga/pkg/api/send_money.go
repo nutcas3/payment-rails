2b4c3450753b8f750946f80f2c4a1f60662361ed
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-22 14:30:27 +0300

    feat(api): add internal bank transfer functionality for Equity Bank accounts

--- jenga/pkg/api/send_money.go
+++ jenga/pkg/api/send_money.go
@@ -11,6 +11,7 @@ const (
 	mobileWalletEndpoint = "/transaction-api/v3.0/remittance/sendmobile"
 	rtgsEndpoint = "/transaction-api/v3.0/remittance/rtgs"
 	swiftEndpoint = "/transaction-api/v3.0/remittance/swift"
+	internalBankTransferEndpoint = "/transaction-api/v3.0/remittance/internalBankTransfer"
 )
 
 func (c *Client) SendMoney(req SendMoneyRequest) (*SendMoneyResponse, error) {
@@ -26,6 +27,10 @@ func (c *Client) SendMoney(req SendMoneyRequest) (*SendMoneyResponse, error) {
 		endpoint = rtgsEndpoint
 	case TransferTypeSWIFT:
 		endpoint = swiftEndpoint
+	case TransferTypeEFT:
+		if req.Source.CountryCode == req.Destination.CountryCode {
+			endpoint = internalBankTransferEndpoint
+		}
 	}
 
 	signatureData := req.Source.AccountNumber + req.Transfer.Amount + req.Transfer.CurrencyCode + req.Transfer.Reference
@@ -61,6 +66,34 @@ func (c *Client) SendToMobileWallet(req MobileWalletRequest) (*MobileWalletRespo
 	var response MobileWalletResponse
 	if err := json.Unmarshal(respBody, &response); err != nil {
 		return nil, fmt.Errorf("error parsing mobile wallet response: %w", err)
+	}
+
+	return &response, nil
+}
+
+// SendInternalBankTransfer sends money between Equity Bank accounts
+// This function is specifically for transfers within Equity Bank across Kenya, Uganda, Tanzania, Rwanda & South Sudan
+func (c *Client) SendInternalBankTransfer(req SendMoneyRequest) (*SendMoneyResponse, error) {
+	if req.Source.CountryCode == "" || req.Source.AccountNumber == "" || 
+	   req.Destination.CountryCode == "" || req.Destination.AccountNumber == "" ||
+	   req.Transfer.Amount == "" || req.Transfer.CurrencyCode == "" || req.Transfer.Reference == "" ||
+	   req.Transfer.Date == "" || req.Transfer.Description == "" {
+		return nil, fmt.Errorf("missing required fields in SendMoneyRequest for internal bank transfer")
+	}
+
+	// Force the transfer type to EFT for internal transfers
+	req.Transfer.Type = TransferTypeEFT
+
+	signatureData := req.Source.AccountNumber + req.Transfer.Amount + req.Transfer.CurrencyCode + req.Transfer.Reference
+
+	respBody, err := c.SendRequest(http.MethodPost, internalBankTransferEndpoint, req, signatureData)
+	if err != nil {
+		return nil, err
+	}
+
+	var response SendMoneyResponse
+	if err := json.Unmarshal(respBody, &response); err != nil {
+		return nil, fmt.Errorf("error parsing internal bank transfer response: %w", err)
 	}
 
 	return &response, nil
