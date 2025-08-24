50e4d69c3d56d15a0695c014bb58e95b0eb642dd
nutcas3 <maurice.nyanja.e@gmail.com>
2025-08-24 14:18:30 +0300

    feat(api): add webhook handling methods and initialize webhook handler in Client

--- jenga/jenga.go
+++ jenga/jenga.go
@@ -1,11 +1,14 @@
 package jenga
 
 import (
+	"fmt"
+	"net/http"
 	"payment-rails/jenga/pkg/api"
 )
 
 type Client struct {
 	apiClient *api.Client
+	webhookHandler *api.WebhookHandler
 }
 
 func NewClient(apiKey, username, password, privateKey, environment string) (*Client, error) {
@@ -16,7 +19,22 @@ func NewClient(apiKey, username, password, privateKey, environment string) (*Cli
 
 	return &Client{
 		apiClient: apiClient,
+		webhookHandler: nil,
 	}, nil
+}
+
+// SetWebhookSecret sets the webhook secret for validating webhook signatures
+func (c *Client) SetWebhookSecret(webhookSecret string) {
+	c.webhookHandler = api.NewWebhookHandler(webhookSecret)
+}
+
+// HandleWebhook processes incoming webhook requests
+func (c *Client) HandleWebhook(w http.ResponseWriter, r *http.Request, handlers api.WebhookHandlers) error {
+	if c.webhookHandler == nil {
+		return fmt.Errorf("webhook handler not initialized, call SetWebhookSecret first")
+	}
+	c.webhookHandler.HandleWebhook(w, r, handlers)
+	return nil
 }
 
 func (c *Client) GetAccountBalance(req api.AccountBalanceRequest) (*api.AccountBalanceResponse, error) {
@@ -53,6 +71,14 @@ func (c *Client) PayBill(req api.BillPaymentRequest) (*api.BillPaymentResponse, 
 	return c.apiClient.PayBill(req)
 }
 
+func (c *Client) ReceiveMoney(req api.ReceiveMoneyRequest) (*api.ReceiveMoneyResponse, error) {
+	return c.apiClient.ReceiveMoney(req)
+}
+
+func (c *Client) QueryReceiveMoneyTransaction(req api.ReceiveMoneyQueryRequest) (*api.ReceiveMoneyQueryResponse, error) {
+	return c.apiClient.QueryReceiveMoneyTransaction(req)
+}
+
 func (c *Client) PurchaseAirtime(req api.AirtimePurchaseRequest) (*api.AirtimePurchaseResponse, error) {
 	return c.apiClient.PurchaseAirtime(req)
 }
@@ -60,6 +86,14 @@ func (c *Client) PurchaseAirtime(req api.AirtimePurchaseRequest) (*api.AirtimePu
 
 func (c *Client) VerifyIdentity(req api.KYCRequest) (*api.KYCResponse, error) {
 	return c.apiClient.VerifyIdentity(req)
+}
+
+func (c *Client) PerformAMLScreening(req api.AMLScreeningRequest) (*api.AMLScreeningResponse, error) {
+	return c.apiClient.PerformAMLScreening(req)
+}
+
+func (c *Client) PerformCustomerDueDiligence(req api.CDDRequest) (*api.CDDResponse, error) {
+	return c.apiClient.PerformCustomerDueDiligence(req)
 }
 
 func (c *Client) GetForexRates(req api.ForexRatesRequest) (*api.ForexRatesResponse, error) {
